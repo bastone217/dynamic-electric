378edcff11f681a81cef190b34d9e6b0df20ee2a
Brook S <55720576+bastone217@users.noreply.github.com>
2020-10-01 09:54:35 -0700

    finalized the about us page.

--- src/screens/aboutUs.js
+++ src/screens/aboutUs.js
@@ -6,36 +6,49 @@ const AboutUs = () =>(
     <div>
          <p>With qualified commercial electricians on our team, <span className='bold'>Dynamic Electrical Contracting & Controls, Inc.</span>
          provides expert commercial installations and repairs in the Central Valley (including but not limited to
-         Fresno, Modesto, Madera, Merced & Atwater). Our Journeyman technicians are certified by the DIR and insured, 
-         and proud to provide an excellent and reliable service. Dynamic Electrical Contracting & Controls, Inc. is 
-         equipped to handle all your commercial electrical needs. Together our team works diligently to maintain our 
-         reputation of staying on schedule. Our Journey men not only work diligently to stay on task but are able to 
-         create solutions to issues that arise while on the job site; to ensure the project stays on task! Dynamic 
-         Electrical Contracting & Controls, Inc offers full-service new construction electrical services for 
-         commercial properties throughout Fresno and the surrounding areas. In addition to new construction, our 
-         electricians can update outdated electrical systems on existing construction sites, performing a variety of 
-         electrical repairs, electrical wiring, lighting installations and more.</p>
+         Fresno, Modesto, Madera, Merced & Atwater).Our Journeyman technicians are certified by the DIR, fully insured, and proud to provide an excellent and
+          reliable service. Dynamic Electrical Contracting & Controls, Inc. is equipped to handle all your commercial electrical needs. 
+          Together our team works diligently to maintain our reputation of staying on schedule. Our Journeymen not only work diligently 
+          to stay on task but are able to create solutions to issues that arise while on the job site; to ensure the project stays on task! 
+          Dynamic Electrical Contracting & Controls, Inc offers full-service new construction electrical services for commercial properties 
+          throughout Atwater, Modesto, Merced, Madera, Fresno and the surrounding areas. In addition to new construction, our electricians can update 
+          outdated electrical systems on existing construction sites, performing a variety of electrical repairs, electrical wiring, lighting installations 
+          and more. </p>
 
          <h4>The services our Team of electricians provide include:</h4>
          <ul>
-             <li>Aluminum Wiring Replacement</li>
+             <li>Underground trenching, conduits, and cabling</li>
+             <li>Horizontal/ Directional drilling</li>
+             <li>Light pole base augering, forming, installation</li>
+             <li>Data/ Communications/ Fiber optic design & installations either self-performed or through network of qualified subcontractors</li>
+             <li>Stadium lighting installations and repairs</li>
+             <li>Agricultura electrical including grounding systems, design, and installation</li>
+             <li>Food industry process electrical</li>
              <li>Electrical Safety Inspections</li>
+             <li>Fire alarm system through qualified network of subcontractors</li>
+             <li>Generator and automatic transfer switch Installation and repair</li>
              <li>Electrical Wiring Repairs</li>
-             <li>Lighting Installations</li>
-             <li>Security and Landscape Lighting</li>
-             <li>Panel Upgrades</li>
-             <li>Surge Protection</li>
-             <li>Ground Fault Circuit Interrupters</li>
+             <li>Lighting Design, layout, and Installations</li>
+             <li>Security Lighting</li>
+             <li>Switchboard and Panel Upgrades</li>
+             <li>UPS system installation and maintenance</li>
+             <li>Design/ Build electrical</li>
+             <li>Surge Protection and breaker installations</li>
              <li>Outlet Repair</li>
              <li>Lighting Repairs</li>
+             <li>Energy generating systems</li>
          </ul>
 
          <h4>Some of the specific commercial electrical services we provide include:</h4>
          <ul>
              <li>Electrical Distribution Systems</li>
-             <li>HVAC Control</li>
-             <li>Lighting and Dimming Systems</li>
+             <li>Emergency Power Systems and Generators</li>
+             <li>Process plant controls and design</li>
              <li>Lighting Protection System</li>
+             <li>Lighting and Dimming Systems to conform to CA Title 24 requirements</li>
+             <li>Lightning Protection System</li>
+             <li>Underground electrical infrastructure</li>
+             <li>Emergency lighting design and upgrades</li>
              <li>Low Voltage Cabling: Voice, Data, TV, POS, Gaming, Security, AV</li>
              <li>UPS Systems</li>
          </ul>
